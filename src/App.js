54d157c7b6eaf525bd9c143bc51479911adfa76f
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-06-25 01:34:15 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="page">
-      <h1>Dharma Pastelería</h1>
+      <h1 className='title'>Dharma Pastelería</h1>
+      <h2 className='subtitle'>Buenos Aires</h2>
+      <div className='categories'>
+        <h2>Tortas</h2>
+        <h2>Tartas</h2>
+        <h2>Salado</h2>
+        <h2>Budines</h2>
+        <h2>Otras</h2>
+      </div>
     </div>
   );
 }

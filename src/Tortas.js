60c7b5d756cde54f2892dd4b5bd25c8888cb1090
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-06-29 23:35:40 -0400

    Add files via upload

--- src/Tortas.js
+++ src/Tortas.js
@@ -7,6 +7,9 @@ import cake1 from './static/torta1.png';
 import cake2 from './static/torta2.png';
 import Praline from './static/tortaPraline.png';
 import FrutosRojos from './static/tortaFrutosRojos.png';
+import Vegana from './static/tortaVegana.png';
+import Nuez from './static/tortaNuez.png';
+import Oreo from './static/tortaOreo.png';
 
 import {useState, useEffect} from 'react';
 
@@ -65,16 +68,35 @@ function Tortas( {setHome, setCakesScreen, setTartasScreen, setSaladoScreen, set
         <h2 className='sectionHeading'>/</h2>
         <h2 className='sectionHeading'>Tortas</h2>
       </div>
-      <h1>LAS TORTAS</h1>
+      <h1 className='sectionTitle'>LAS TORTAS</h1>
       <div className='sectionImages'>
         <div className='sectionImagesTop'>
           <div>
             <img src={Praline} className="sectionIMG" alt="Praline"/>
-            <h1>Torta Praline</h1>
+            <h1 className='itemName'>Torta Praline</h1>
+            <h1 className='itemPrice'>$2.500</h1>
           </div>
           <div>
             <img src={FrutosRojos} className="sectionIMG" alt="Frutos Rojos"/>
-            <h1>Torta Frutos Rojos</h1>
+            <h1 className='itemName'>Torta Frutos Rojos</h1>
+            <h1 className='itemPrice'>$3.500</h1>
+          </div>
+          <div>
+            <img src={Vegana} className="sectionIMG" alt="Vegana"/>
+            <h1 className='itemName'>Torta Vegana</h1>
+            <h1 className='itemPrice'>$3.000</h1>
+          </div>
+        </div>
+        <div className='sectionImagesBottom'>
+          <div>
+            <img src={Nuez} className="sectionIMG" alt="Nuez"/>
+            <h1 className='itemName'>Torta Nuez</h1>
+            <h1 className='itemPrice'>$2.700</h1>
+          </div>
+          <div>
+            <img src={Oreo} className="sectionIMG" alt="Oreo"/>
+            <h1 className='itemName'>Torta Oreo</h1>
+            <h1 className='itemPrice'>$2.550</h1>
           </div>
         </div>
       </div>
